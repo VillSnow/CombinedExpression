3da4a0d9f5294340a1ea00a2c10f9878cce958f4
VillSnow <vill.snow@gmail.com>
2019-02-06 20:25:20 +0900

    add: ChangeParameterName

---
+++ CombinedExpression.UnitTests/UnitTest2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CombinedExpression.UnitTests
+{
+	[TestClass]
+	public class UnitTest2
+	{
+		public TestContext TestContext { get; set; }
+
+		[TestMethod]
+		public void TestMethod1() {
+			var random = new Random();
+			foreach (var _ in Enumerable.Range(0, 10)) {
+				int a = random.Next(-100, 100);
+				TestContext.WriteLine($"a={a}");
+				Expression<Func<int, int, int>> expr1 = (x, y) => x + y;
+				var expr2 = (Expression<Func<int, int>>)expr1.ChangeParameterName("y", "x");
+
+				Assert.AreEqual(2*a, expr2.Compile().Invoke(a));
+			}
+		}
+
+		[TestMethod]
+		public void TestMethod2() {
+			var random = new Random();
+			foreach (var _ in Enumerable.Range(0, 10)) {
+				int a = random.Next(-100, 100);
+				TestContext.WriteLine($"a={a}");
+				Expression<Func<int, int>> expr1 = x => x + 1;
+				Expression<Func<int, int>> expr2 = y => y - 1;
+				Expression<Func<int, int, int>> expr3 = (x, y) => (x + 10) * y;
+				var expr4 = expr3.Composite<int, int>(expr1, expr2.ChangeParameterName("y", "x"));
+
+				Assert.AreEqual((a + 11) * (a - 1), expr4.Compile().Invoke(a));
+			}
+		}
+	}
+}
--- CombinedExpression/Parameter.cs
+++ CombinedExpression/Parameter.cs
@@ -9,11 +9,39 @@ namespace CombinedExpression
 	public partial class ExpressionOperators
 	{
 
+		public static LambdaExpression ChangeParameterName(this LambdaExpression expression, string before, string after) {
+			return ChangeParameterNameImpl(expression, s => s == before ? after : s);
+		}
 
+		public static LambdaExpression ChangeParameterNameImpl(LambdaExpression expression, Func<string,string> selector) {
+			var list = new List<KeyValuePair<ParameterExpression, ParameterExpression>>();
+			var ps = new List<ParameterExpression>();
+			foreach (var item in expression.Parameters) {
+				var after = selector(item.Name);
+				var exist = list.Where(pair => pair.Value.Name == after).Select(pair => pair.Value).FirstOrDefault();
+				if (exist != null) {
+					if (item.Type != exist.Type) {
+						throw new ArgumentException("");
+					}
+					list.Add(new KeyValuePair<ParameterExpression, ParameterExpression>(item, exist));
+				} else {
+					var p = Expression.Parameter(item.Type, after);
+					list.Add(new KeyValuePair<ParameterExpression, ParameterExpression>(item, p));
+					ps.Add(p);
+				}
+			}
+			var visitor = new MyVisitor();
+			visitor.Selector = p => list.Where(pair => pair.Key == p).Select(pair => pair.Value).Single();
+			return Expression.Lambda(visitor.Visit(expression.Body), ps);
+		}
 
 		class MyVisitor : ExpressionVisitor
 		{
-			public Func<ParameterExpression, ParameterExpression> Selector { get; } = p => p;
+			public Func<ParameterExpression, ParameterExpression> Selector { get; set; } 
+
+			public MyVisitor() {
+				Selector = p => p;
+			}
 
 			public MyVisitor(IEnumerable<ParameterExpression> parameters) {
 				if (!(parameters is IReadOnlyList<ParameterExpression>)) {
